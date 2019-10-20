1082ef54fe6fca4520528d9c15aadde531dabb53
Harris Qureshi <catalyst1@gmail.com>
2019-10-20 19:44:51 +0500

    first one

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const center = { 
+   lat: 52.5159, 
+   lng: 13.3777, 
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   id: 'f3v2C5OWemDksnzImxps',
+   code: 'wFVLyjwEf7Q4u6GyNXJvjA',
+   apikey: 'dI6CfkqAFA8aDHtZkN6ajpE0FnmvlkCSoOIYXLQOORQ'
+}
+
+export { center, hereCredentials };
